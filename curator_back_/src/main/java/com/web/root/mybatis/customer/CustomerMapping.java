5a4eb22f677385438d4fb29c756420e3d89775a6
Administrator <Administrator@DESKTOP-2VLVNHN>
2023-06-01 13:52:47 +0900

    customerHelplist

--- curator_back_/src/main/java/com/web/root/mybatis/customer/CustomerMapping.java
+++ curator_back_/src/main/java/com/web/root/mybatis/customer/CustomerMapping.java
@@ -8,5 +8,9 @@ import com.web.root.customer.dto.CustomerDTO;
 public interface CustomerMapping {
 	
 	public List<CustomerDTO> purchaseProduct(Map<String, Object> map);
+	
+	public int customerHelpList(int memberSeq);
+	
+	public Map<String, Object> customerReplyList(int memberSeq);
 
 }
