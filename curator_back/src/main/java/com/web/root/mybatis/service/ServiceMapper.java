073919428d3693c669e571ecf804219979fc1c27
Administrator <Administrator@DESKTOP-2VLVNHN>
2023-06-01 09:07:41 +0900

    commission, service, artist

---
+++ curator_back/src/main/java/com/web/root/mybatis/service/ServiceMapper.java
@@ -0,0 +1,11 @@
+package com.web.root.mybatis.service;
+
+import java.util.List;
+
+import com.web.root.service.dto.ServiceDTO;
+
+public interface ServiceMapper {
+	
+	public List<ServiceDTO> helpAllList();
+
+}
